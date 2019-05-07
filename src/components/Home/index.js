ac4475156ef41700685519b348a3596192f30718
shwe yee <yonngelay@gmail.com>
2019-05-07 14:02:44 +0800

    Adding slider in home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,25 +6,24 @@ import SliderWidget from '../Utils/Slider';
 import { URL_Home } from '../Utils/paths';
 
 class Home extends Component {
-    state = {
-        home: ''
-    }
-    componentDidMount(){
-        axios.get(URL_Home)
-        .then(response => {
-            console.log(response.data);
-            this.setState({
-                home: response.data
-            })
-        })
-    }
-    render(){
-        return(
-            <div>
-                <SliderWidget slides={this.state.home.slider}/>
-            </div>
-        )
-    }
+  state = {
+    home: ''
+  };
+  componentDidMount() {
+    axios.get(URL_Home).then(response => {
+      //console.log(response.data);
+      this.setState({
+        home: response.data
+      });
+    });
+  }
+  render() {
+    return (
+      <>
+        <SliderWidget slides={this.state.home.slider} />
+      </>
+    );
+  }
 }
 
 export default Home;
